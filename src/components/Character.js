7f07d0a5577bfb9c6e3ffa34d431f20bf013de6c
jaredhall18 <jaredhall18@gmail.com>
2021-09-10 15:02:26 -0600

    added styling to characterInfo and container

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,4 +1,29 @@
 import React, {useState, useEffect} from 'react';
+import styled from 'styled-components'
+
+const CharacterInfo = styled.div`
+    display:block;
+    background: rgba(255, 189, 114, .5);
+    padding:20px;
+    width: 25%;
+    /* opacity: 50%; */
+    margin:10px;
+
+    h2, h3 {
+        color: black;
+    }
+
+    
+
+`
+
+const CharacterContainer = styled.div`
+display:flex;
+justify-content:center;
+flex-wrap: wrap;
+`
+
+
 
 
 const Character = (props) => {
@@ -8,11 +33,14 @@ const Character = (props) => {
         <div>
              {characters.map((res, index) => {
                  return(
-                     <div key = {index}>
-                        <h1>{res.name}</h1>
-                        <h2>{res.gender}</h2>
-                        <h2>{res.homeworld}</h2>
-                     </div>
+                     <CharacterContainer key = {index}>
+                        <CharacterInfo>
+                            <h2>{res.name}</h2>
+                            <h3>Gender: {res.gender}</h3>
+                            <h3>Height: {res.height}</h3>
+                        </CharacterInfo>
+                        
+                     </CharacterContainer>
                  )
              })}
         </div>
