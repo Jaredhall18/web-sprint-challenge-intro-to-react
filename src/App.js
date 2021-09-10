376071d5af882e5cb25dafb201cc7c053f9e778b
jaredhall18 <jaredhall18@gmail.com>
2021-09-10 10:26:31 -0600

    accessed API to pull character data down to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  //Bulk Character list from API
+  const [characters, setCharacters] = useState([]);
+//This will be for individual character info
+  const [character, setCharacter] = useState(null);
+
+ //I may need to create open and closeDetails here
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`).then(res => {
+      setCharacters(res.data);
+      console.log(res.data);
+    }).catch(err => console.error(err));
+  }, [])
+
 
   return (
     <div className="App">
